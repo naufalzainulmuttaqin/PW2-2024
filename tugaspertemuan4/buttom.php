a8ae4701d979ac5892707a63743c08ffd2464fc1
naufalzainulmuttaqin <naufalzainul473@gmail.com>
2024-03-18 18:09:30 +0700

    push pertemuan keempat

---
+++ tugaspertemuan4/buttom.php
@@ -0,0 +1,33 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; Naufal ZM</strong>
+    All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="template/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="template/dist/js/adminlte.js"></script>
+
+<!-- PAGE template/PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="template/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="template/plugins/raphael/raphael.min.js"></script>
+<script src="template/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="template/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="template/plugins/chart.js/Chart.min.js"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="template/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="template/dist/js/pages/dashboard2.js"></script>
+</body>
+
+</html>
---
+++ tugaspertemuan4/index.php
@@ -0,0 +1,39 @@
+<?php 
+    include_once('top.php');
+    include_once('menu.php');
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard</h1>
+            <p class="m-0">SELAMAT DATANG DIDASHBOARD</p>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v2</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+
+<?php 
+  include_once('buttom.php');
+?>
